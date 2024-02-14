ecd3064a6cf3c16cace0d473c984d626763222a2
danpanfili <daniel.paul.panfili@gmail.com>
2024-02-14 12:08:24 -0600

    Update mtg_proxy.py

--- mtg_proxy.py
+++ mtg_proxy.py
@@ -10,7 +10,7 @@ print(f"Loading deck at path: {src.deck.PATH}")
 
 if '--update' in args or '-u' in args: src.database.Update()
 
-if src.database.isURL: myDeck = src.deck.Load({'name': args[-1].split('/')[-1]})
+if src.deck.isURL: myDeck = src.deck.Load({'name': args[-1].split('/')[-1]})
 else: myDeck = src.deck.Load({'name': src.deck.PATH.replace('.txt','')})
 
 pdf = src.pdf.Make(myDeck)
